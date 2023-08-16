3addf960bf03db74e80d19a4aebbaa50bfa4416d
AmarAuto <amarjeet.sahoo@gmail.com>
2023-08-16 13:55:10 +0530

    first commit for Cucumber Login

---
+++ src/test/resource/AppFeatures/AccountsPage.feature
@@ -0,0 +1,23 @@
+Feature: Account Page Feature
+
+Background:
+Given user has already logged in to application
+|username|password|
+|dec2020secondbatch@gmail.com|Login@123|
+
+Scenario: Accounts page title
+Given user is on Accounts page
+When user gets the title of the page
+Then page title should be "My account - My Shop"
+
+Scenario: Accounts section count
+Given user is on Accounts page
+Then user gets accounts section
+|ADD MY FIRST ADDRESS|
+|ORDER HISTORY AND DETAILS|
+|MY CREDIT SLIPS|
+|MY ADDRESSES|
+|MY PERSONAL INFORMATION|
+|Home|
+And accounts section count should be 6
+
---
+++ test-output-thread/report.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "9fd4fd58-d0ef-49b5-b067-c79616b7baba",
+    "feature": "Account Page Feature",
+    "scenario": "Accounts page title",
+    "start": 1691831269719,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1691831279829,
+    "className": "passed"
+  },
+  {
+    "id": "e0cfac4f-2fd3-4179-b859-c01250c81fc1",
+    "feature": "Login page feature",
+    "scenario": "Forgot Password link",
+    "start": 1691831295372,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1691831301764,
+    "className": "passed"
+  },
+  {
+    "id": "e9de228f-5bf5-48ce-ae76-08c1a7646423",
+    "feature": "Login page feature",
+    "scenario": "Login with correct credentials",
+    "start": 1691831301772,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1691831309412,
+    "className": "passed"
+  },
+  {
+    "id": "e6a89c84-3804-49b5-b4a2-82e6f1e4e3cf",
+    "feature": "Account Page Feature",
+    "scenario": "Accounts section count",
+    "start": 1691831279841,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1691831288630,
+    "className": "passed"
+  },
+  {
+    "id": "03b0db33-c598-4a64-85f9-c7906b32f3ed",
+    "feature": "Login page feature",
+    "scenario": "Login page title",
+    "start": 1691831288635,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1691831295366,
+    "className": "passed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 1,
+    "content": "Thread[#1,main,5,main]"
+  }
+]);
+});
